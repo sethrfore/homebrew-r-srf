0c2373c44df4eea82d6957eb3c6d492289b019cd
sethrfore <seth.fore@icloud.com>
2017-12-18 13:50:24 -0600

    SRF - r.rb: Add customized homebrew r.rb formula

---
+++ r.rb
@@ -0,0 +1,135 @@
+class R < Formula
+  desc "Software environment for statistical computing"
+  homepage "https://www.r-project.org/"
+  url "https://cran.rstudio.com/src/base/R-3/R-3.4.3.tar.gz"
+  sha256 "7a3cb831de5b4151e1f890113ed207527b7d4b16df9ec6b35e0964170007f426"
+
+  bottle do
+    sha256 "5980c32c37d78545b85a745af26688c4b9b38748086597e9476d19782cab9200" => :high_sierra
+    sha256 "7362e6983438c23dd79cd5461836111da9c404c1b328be87226a0e55102cd070" => :sierra
+    sha256 "c8c3f89cba5a1bbc383ddacfba6b9dcdebe576fa9112dd30bacf390b8f67768a" => :el_capitan
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "pcre"
+  depends_on "readline"
+  depends_on "xz"
+  depends_on :fortran
+  depends_on "openblas" => :optional
+  depends_on :java => :optional
+
+  ## SRF
+  depends_on :tex
+  depends_on "texinfo"
+  depends_on "libtiff"
+  depends_on :x11
+  depends_on "cairo"
+  depends_on "icu4c"
+  depends_on "pango"
+
+
+  # needed to preserve executable permissions on files without shebangs
+  skip_clean "lib/R/bin"
+
+  resource "gss" do
+    url "https://cloud.r-project.org/src/contrib/gss_2.1-7.tar.gz", :using => :nounzip
+    mirror "https://mirror.las.iastate.edu/CRAN/src/contrib/gss_2.1-7.tar.gz"
+    sha256 "0405bb5e4c4d60b466335e5da07be4f9570045a24aed09e7bc0640e1a00f3adb"
+  end
+
+  def install
+    # Fix dyld: lazy symbol binding failed: Symbol not found: _clock_gettime
+    if MacOS.version == "10.11" && MacOS::Xcode.installed? &&
+       MacOS::Xcode.version >= "8.0"
+      ENV["ac_cv_have_decl_clock_gettime"] = "no"
+    end
+
+	## SRF
+	# Fix cairo detection with Quartz-only cairo
+	inreplace ["configure", "m4/cairo.m4"], "cairo-xlib.h", "cairo.h"
+
+
+    args = [
+      "--prefix=#{prefix}",
+      "--enable-memory-profiling",
+      "--with-cairo",
+      "--with-aqua",
+      "--with-lapack",
+      "--enable-R-shlib",
+      "SED=/usr/bin/sed", # don't remember Homebrew's sed shim,
+      "--with-tcltk",
+      "--with-tcl-config=/System/Library/Frameworks/Tcl.framework/tclConfig.sh",
+      "--with-tk-config=/System/Library/Frameworks/Tk.framework/tkConfig.sh"
+    ]
+
+    if build.with? "openblas"
+      args << "--with-blas=-L#{Formula["openblas"].opt_lib} -lopenblas"
+      ENV.append "LDFLAGS", "-L#{Formula["openblas"].opt_lib}"
+    else
+      args << "--with-blas=-framework Accelerate"
+      ENV.append_to_cflags "-D__ACCELERATE__" if ENV.compiler != :clang
+    end
+
+    if build.with? "java"
+      args << "--enable-java"
+    else
+      args << "--disable-java"
+    end
+
+    # Help CRAN packages find gettext and readline
+    ["gettext", "readline"].each do |f|
+      ENV.append "CPPFLAGS", "-I#{Formula[f].opt_include}"
+      ENV.append "LDFLAGS", "-L#{Formula[f].opt_lib}"
+    end
+
+    system "./configure", *args
+    system "make"
+    ENV.deparallelize do
+      system "make", "install"
+    end
+
+    cd "src/nmath/standalone" do
+      system "make"
+      ENV.deparallelize do
+        system "make", "install"
+      end
+    end
+
+    r_home = lib/"R"
+
+    # make Homebrew packages discoverable for R CMD INSTALL
+    inreplace r_home/"etc/Makeconf" do |s|
+      s.gsub!(/^CPPFLAGS =.*/, "\\0 -I#{HOMEBREW_PREFIX}/include")
+      s.gsub!(/^LDFLAGS =.*/, "\\0 -L#{HOMEBREW_PREFIX}/lib")
+      s.gsub!(/.LDFLAGS =.*/, "\\0 $(LDFLAGS)")
+    end
+
+    include.install_symlink Dir[r_home/"include/*"]
+    lib.install_symlink Dir[r_home/"lib/*"]
+
+    # avoid triggering mandatory rebuilds of r when gcc is upgraded
+    inreplace lib/"R/etc/Makeconf", Formula["gcc"].prefix.realpath,
+                                    Formula["gcc"].opt_prefix
+  end
+
+  def post_install
+    short_version =
+      `#{bin}/Rscript -e 'cat(as.character(getRversion()[1,1:2]))'`.strip
+    site_library = HOMEBREW_PREFIX/"lib/R/#{short_version}/site-library"
+    site_library.mkpath
+    ln_s site_library, lib/"R/site-library"
+  end
+
+  test do
+    assert_equal "[1] 2", shell_output("#{bin}/Rscript -e 'print(1+1)'").chomp
+    assert_equal ".dylib", shell_output("#{bin}/R CMD config DYLIB_EXT").chomp
+
+    testpath.install resource("gss")
+    system bin/"R", "CMD", "INSTALL", "--library=.", Dir["gss*"].first
+    assert_predicate testpath/"gss/libs/gss.so", :exist?,
+                     "Failed to install gss package"
+  end
+end
