df9fb8e63f2d74243e711dfcebc31db927af91f4
sethrfore <seth.fore@icloud.com>
2018-05-08 08:19:17 -0500

    SRF - cairo.rb: Remove bottle to force source install. This ensures cairo builds with X11 support

--- cairo.rb
+++ cairo.rb
@@ -5,11 +5,11 @@ class Cairo < Formula
   mirror "https://www.mirrorservice.org/sites/ftp.netbsd.org/pub/pkgsrc/distfiles/cairo-1.14.12.tar.xz"
   sha256 "8c90f00c500b2299c0a323dd9beead2a00353752b2092ead558139bd67f7bf16"
 
-  bottle do
-    sha256 "5bdc28de8e5a615ab664d43f7f322ed02d58071171415bb6e2750f486b9465e2" => :high_sierra
-    sha256 "102847d74a0a11bb6143d93b9f32e1736e88036fb4c685d554a8bcd376bbd929" => :sierra
-    sha256 "bec85433a35605164bdbf5f8913e29eb6d9ceb5acc5569dd9d864706ae6c8d49" => :el_capitan
-  end
+  # bottle do
+  #   sha256 "5bdc28de8e5a615ab664d43f7f322ed02d58071171415bb6e2750f486b9465e2" => :high_sierra
+  #   sha256 "102847d74a0a11bb6143d93b9f32e1736e88036fb4c685d554a8bcd376bbd929" => :sierra
+  #   sha256 "bec85433a35605164bdbf5f8913e29eb6d9ceb5acc5569dd9d864706ae6c8d49" => :el_capitan
+  # end
 
   head do
     url "https://anongit.freedesktop.org/git/cairo", :using => :git
