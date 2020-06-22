1f6a54c06cf1b26976c4165a25790ffd93b14f02
Luis Puerto <luis@luispuerto.net>
2020-06-22 13:22:46 +0300

    Update r.rb

--- r.rb
+++ r.rb
@@ -1,8 +1,8 @@
 class R < Formula
   desc "Software environment for statistical computing"
   homepage "https://www.r-project.org/"
-  url "https://cloud.r-project.org/src/base/R-4/R-4.0.1.tar.gz"
-  sha256 "95fe24a4d8d8f8f888460c8f5fe4311cec656e7a1722d233218bc03861bc6f32"
+  url "https://cloud.r-project.org/src/base/R-4/R-4.0.2.tar.gz"
+  sha256 "d3bceab364da0876625e4097808b42512395fdf41292f4915ab1fd257c1bbe75"
 
   depends_on "pkg-config" => :build
   depends_on "gcc" # for gfortran
