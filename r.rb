3855349b251abffe7352b6cac40a2433a447c363
sethrfore <seth.fore@icloud.com>
2020-12-20 07:58:17 -0600

    SRF - r.rb: Xll update

--- r.rb
+++ r.rb
@@ -9,39 +9,41 @@ class R < Formula
   depends_on "gettext"
   depends_on "jpeg"
   depends_on "libpng"
-  depends_on "openblas" => :optional
   depends_on "pcre2"
   depends_on "readline"
   depends_on "xz"
 
   ## SRF - Add additional R capabilities (comment out if undesired)
+  ## - X11 necessary for tcl-tk since tk.h includes X11 headers. See section A.2.1 Tcl/Tk at < https://cran.r-project.org/doc/manuals/r-release/R-admin.html >
+  # depends_on :x11
+  depends_on "freetype"
+  depends_on "fontconfig"
+  depends_on "libx11"
+  depends_on "libxt"
+  depends_on "libxext"
+  depends_on "libxmu"
+  ## - Cairo must be build with with X11 support. Use brew install sethrfore/r-srf/cairo
+  depends_on "sethrfore/r-srf/cairo" => :optional 
+  depends_on "openblas" => :optional
   depends_on "openjdk" => :optional
-  depends_on :x11 # SRF - X11 necessary for tcl-tk since tk.h includes X11 headers. See section A.2.1 Tcl/Tk at < https://cran.r-project.org/doc/manuals/r-release/R-admin.html >
   depends_on "texinfo" => :optional
   depends_on "libtiff" => :optional
-  depends_on "sethrfore/r-srf/cairo" => :optional # SRF - Cairo must be build with with X11 support. Use brew install sethrfore/r-srf/cairo
   depends_on "icu4c" => :optional
   # depends_on "pango" => :optional
-  #depends_on "tcl-tk" 
-  # needed to preserve executable permissions on files without shebangs
-  
+  # depends_on "tcl-tk"
+
+  ## Needed to preserve executable permissions on files without shebangs
   skip_clean "lib/R/bin", "lib/R/doc"
 
-  resource "gss" do
-    url "https://cloud.r-project.org/src/contrib/gss_2.2-2.tar.gz", :using => :nounzip
-    mirror "https://cran.r-project.org/src/contrib/gss_2.2-2.tar.gz"
-    sha256 "1da4da894378ee730cff9628e8b4d2a0d7dfa344b94e5bce6953e66723c21fe4"
-  end
-
   def install
-    # Fix dyld: lazy symbol binding failed: Symbol not found: _clock_gettime
+    ## Fix dyld: lazy symbol binding failed: Symbol not found: _clock_gettime
     if MacOS.version == "10.11" && MacOS::Xcode.installed? &&
        MacOS::Xcode.version >= "8.0"
       ENV["ac_cv_have_decl_clock_gettime"] = "no"
     end
 
     ## YT - enable tcl-tk support using system headers
-    if MacOS.version == "10.15" || MacOS.version == "11.0" # This seems to work for Catalina and Big Sur
+    if MacOS.version >= "10.15" 
       ## YT - Set up some  environment variables and over-write some variables defined in tclConfig.sh and tkConfig.sh 
       ENV["TCL_INCLUDE_SPEC"] = "-I#{MacOS.sdk_path}/System/Library/Frameworks/Tcl.framework/Versions/8.5/Headers"
       ENV["TK_INCLUDE_SPEC"] = "-I#{MacOS.sdk_path}/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers"
@@ -51,27 +53,29 @@ class R < Formula
                -F#{MacOS.sdk_path}/System/Library/Frameworks -framework Tcl"
     end
 
+    ## SRF - Add Tex to path, uncomment if mactex is installed and desired
+    # ENV.append_path "PATH", "/Library/TeX/texbin"
+
     ## YT - If homebrew's tcl-tk is to be used, this line should be uncommented
     #tcl_lib = Formula["tcl-tk"].opt_lib
+    
     args = [
       "--prefix=#{prefix}",
       "--enable-memory-profiling",
       "--with-x", # SRF - Add X11 support (comment --without-x). Necessary for tcl-tk support.
-      #"--without-x",  # YT - If Homebrew's tcl-tk is to be used, '--with-x' cause an error 
+      #"--without-x", # YT - If Homebrew's tcl-tk is to be used, '--with-x' cause an error.
       "--with-aqua",
       "--with-lapack",
       "--enable-R-shlib",
       "SED=/usr/bin/sed", # don't remember Homebrew's sed shim
       "--with-tcltk", # SRF - Add tcl-tk support.
       "--with-tcl-config=#{MacOS.sdk_path}/System/Library/Frameworks/Tcl.framework/tclConfig.sh",
+      "--with-tk-config=#{MacOS.sdk_path}/System/Library/Frameworks/Tk.framework/tkConfig.sh",
       #"--with-tcl-config=#{tcl_lib}/tclConfig.sh", # YT - If homebrew's tcl-tk is to be used, this line should be uncommented
       #"--with-tk-config=#{tcl_lib}/tkConfig.sh" # YT - If homebrew's tcl-tk is to be used, this line should be uncommented
-      "--with-tk-config=#{MacOS.sdk_path}/System/Library/Frameworks/Tk.framework/tkConfig.sh",
     ]
     
-    ### Tests for tcltk
-    #ENV["TCL_INCLUDE_SPEC"] = "-iwithsysroot #{MacOS.sdk_path}/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers"
-
+    ## SRF - Add supporting flags for optional packages
     if build.with? "openblas"
       args << "--with-blas=-L#{Formula["openblas"].opt_lib} -lopenblas"
       ENV.append "LDFLAGS", "-L#{Formula["openblas"].opt_lib} -lopenblas"
@@ -86,23 +90,21 @@ class R < Formula
       args << "--disable-java"
     end
 
-    ## SRF - Add Cairo support
     if build.with? "cairo"
       args << "--with-cairo"
     else
       args << "--without-cairo"
     end
 
-    # Help CRAN packages find gettext and readline
-    ["gettext", "readline"].each do |f|
-      ENV.append "CPPFLAGS", "-I#{Formula[f].opt_include}"
-      ENV.append "LDFLAGS", "-L#{Formula[f].opt_lib}"
-    end
-
-    ## SRF - Help CRAN packages find icu4c (e.g. rJava)
     if build.with? "icu4c"
       ENV.append "CPPFLAGS", "-I#{Formula["icu4c"].opt_include}"
       ENV.append "LDFLAGS", "-L#{Formula["icu4c"].opt_lib}"
+    end
+
+    # Help CRAN packages find gettext and readline
+    ["gettext", "readline", "xz"].each do |f|
+      ENV.append "CPPFLAGS", "-I#{Formula[f].opt_include}"
+      ENV.append "LDFLAGS", "-L#{Formula[f].opt_lib}"
     end
 
     system "./configure", *args
@@ -146,9 +148,9 @@ class R < Formula
   test do
     assert_equal "[1] 2", shell_output("#{bin}/Rscript -e 'print(1+1)'").chomp
     assert_equal ".dylib", shell_output("#{bin}/R CMD config DYLIB_EXT").chomp
+    # assert_equal "[1] \"aqua\"", shell_output("#{bin}/Rscript -e 'library(tcltk)' -e 'tclvalue(.Tcl(\"tk windowingsystem\"))'").chomp
 
-    testpath.install resource("gss")
-    system bin/"R", "CMD", "INSTALL", "--library=.", Dir["gss*"].first
+    system bin/"Rscript", "-e", "install.packages('gss', '.', 'https://cloud.r-project.org')"
     assert_predicate testpath/"gss/libs/gss.so", :exist?,
                      "Failed to install gss package"
   end
