b6e254e4b03c42bdebf6d0489012eccb2f02ef9a
Yukio Takeuchi <yukio_takeuchi@icloud.com>
2021-08-16 11:22:12 +0900

    update to R-4.1.1

--- r.rb
+++ r.rb
@@ -1,10 +1,10 @@
 class R < Formula
   desc "Software environment for statistical computing"
   homepage "https://www.r-project.org/"
-  url "https://cran.r-project.org/src/base/R-4/R-4.1.0.tar.gz"
-  sha256 "e8e68959d7282ca147360fc9644ada9bd161bab781bab14d33b8999a95182781"
+  url "https://cran.r-project.org/src/base/R-4/R-4.1.1.tar.gz"
+  sha256 "515e03265752257d0b7036f380f82e42b46ed8473f54f25c7b67ed25bbbdd364"
   license "GPL-2.0-or-later"
-  revision 2
+  #revision 2
 
   depends_on "pkg-config" => :build
   depends_on "fontconfig"
