5d9f89f695236ff75e97cd46f685b4735a04a61c
sethrfore <seth.fore@icloud.com>
2021-08-17 08:45:07 -0500

    SRF-r.rb: update 4.1.1

--- r.rb
+++ r.rb
@@ -1,8 +1,8 @@
 class R < Formula
   desc "Software environment for statistical computing"
   homepage "https://www.r-project.org/"
-  url "https://cran.r-project.org/src/base/R-4/R-4.1.0.tar.gz"
-  sha256 "e8e68959d7282ca147360fc9644ada9bd161bab781bab14d33b8999a95182781"
+  url "https://cran.r-project.org/src/base/R-4/R-4.1.1.tar.gz"
+  sha256 "515e03265752257d0b7036f380f82e42b46ed8473f54f25c7b67ed25bbbdd364"
   license "GPL-2.0-or-later"
   revision
 
@@ -46,7 +46,7 @@ class R < Formula
       "--with-blas=-L#{Formula["openblas"].opt_lib} -lopenblas",
       "--enable-R-shlib",
       "--enable-java",
-      "SED=/usr/bin/sed", # don't remember Homebrew's sed shim
+      "--with-cairo"
     ]
 
     # Help CRAN packages find gettext and readline
