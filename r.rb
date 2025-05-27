89349c8b49e21c3d956df6c49a0a0d5f633b2ed0
sethrfore <seth.fore@icloud.com>
2025-05-27 15:14:54 -0500

    SRF - r.rb: Version update 4.5.0. ** Removed previous edits based on updated/patched Homebrew formula. Remove patch when fixed. **

--- r.rb
+++ r.rb
@@ -1,13 +1,14 @@
 class R < Formula
   desc "Software environment for statistical computing"
   homepage "https://www.r-project.org/"
+  url "https://cran.r-project.org/src/base/R-4/R-4.5.0.tar.gz"
+  sha256 "3b33ea113e0d1ddc9793874d5949cec2c7386f66e4abfb1cef9aec22846c3ce1"
   ## SRF - R 4.5.0 currently does not install properly, using patched version below
-  # url "https://cran.r-project.org/src/base/R-4/R-4.5.0.tar.gz"
-  # sha256 "3b33ea113e0d1ddc9793874d5949cec2c7386f66e4abfb1cef9aec22846c3ce1"
-  url "https://cran.r-project.org/src/base-prerelease/R-patched.tar.gz"
-  sha256 "0d08b773ebd36bca0f71e11bdb3cbd9ff2b966c48fb3b2c5b7e0776802cd79bf"
-  version "4.5.0p"
+  # url "https://cran.r-project.org/src/base-prerelease/R-patched.tar.gz"
+  # sha256 "0d08b773ebd36bca0f71e11bdb3cbd9ff2b966c48fb3b2c5b7e0776802cd79bf"
+  # version "4.5.0p"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url "https://cran.rstudio.com/banner.shtml"
@@ -65,6 +66,9 @@ class R < Formula
   ## Needed to preserve executable permissions on files without shebangs
   skip_clean "lib/R/bin", "lib/R/doc"
 
+  # Fix build with clang 17
+  # https://github.com/wch/r-source/commit/489a6b8d330bb30da82329f1949f44a0f633f1e8
+  patch :DATA
 
   def install
     ## SRF - Add Tex to path, uncomment if mactex is installed and desired
@@ -177,3 +181,28 @@ class R < Formula
                  shell_output("#{bin}/Rscript -e 'library(tcltk)' -e 'tclvalue(.Tcl(\"tk windowingsystem\"))'").chomp
   end
 end
+__END__
+diff -pur R-4.5.0/src/nmath/mlutils.c R-4.5.0-patched/src/nmath/mlutils.c
+--- R-4.5.0/src/nmath/mlutils.c 2025-03-14 00:02:15
++++ R-4.5.0-patched/src/nmath/mlutils.c 2025-05-24 12:16:15
+@@ -105,7 +105,20 @@ double R_pow_di(double x, int n)
+     return pow;
+ }
+
++/* It is not clear why these are being defined in standalone nmath:
++ * but that they are is stated in the R-admin manual.
++ *
++ * In R NA_AREAL is a specific NaN computed during initialization.
++ */
++#if defined(__clang__) && defined(NAN)
++// C99 (optionally) has NAN, which is a float but will coerce to double.
++double NA_REAL = NAN;
++#else
++// ML_NAN is defined as (0.0/0.0) in nmath.h
++// Fails to compile in Intel ics 2025.0, Apple clang 17, LLVM clang 20
+ double NA_REAL = ML_NAN;
++#endif
++
+ double R_PosInf = ML_POSINF, R_NegInf = ML_NEGINF;
+
+ #include <stdio.h>
